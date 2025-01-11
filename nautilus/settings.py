d8b1b10de6e748fba434eb6a94f4d35bc44ad465
Šimon Hlavsa <simon.hlavse55@gmail.com>
2025-01-11 01:59:47 +0100

    configure static file handling with Whitenoise middleware and update STATIC_URL

--- nautilus/settings.py
+++ nautilus/settings.py
@@ -148,13 +148,15 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "main" / "static",
+# Middleware
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
